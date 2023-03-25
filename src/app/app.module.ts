12eba352c2f3cf2c662a87e78407447cdd155fdb
Jonathan Payne <jonathnp@bellsouth.net>
2023-03-25 12:31:28 -0400

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PtHeaderComponent } from './pt-header/pt-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PtHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pt-header/pt-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pt-header',
+  templateUrl: './pt-header.component.html',
+  styleUrls: ['./pt-header.component.scss']
+})
+export class PtHeaderComponent {
+
+}
